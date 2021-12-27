fb8c2b0222364d683dfdf447e765abcda7556301
Ian-SAfc <78266111+Ian-SAfc@users.noreply.github.com>
2021-12-27 11:17:35 -0500

    Update MColor_Functions.py

--- MColor_Functions.py
+++ MColor_Functions.py
@@ -1,6 +1,9 @@
 
-#Takes a KivyMD color statement and converts it into #999999
+#Takes a KivyMD color statement and converts it into hex format e.g. #FF00FF
 #This is Rgba to Hex essentially.  We ignore the [a]lpha.
+#For example you can pass self.theme_cls.primary_color and it will convert it to hex
+#I've written this code out "longhand".  You can acheive the same result in half the lines,
+#I just wanted to make the formula clearer. 
 
 def MTupleToHexColor(MRgba_value):
 	Mfirst_hex = int(MRgba_value[0] * 256)
